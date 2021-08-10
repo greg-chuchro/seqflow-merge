00007b105fd5427806d52a4f208507f996dd9db1
greg-chuchro <gchuchro@acm.org>
2021-08-10 23:33:04 +0900

    #7 merge changes ignored by synver

--- action.sh
+++ action.sh
@@ -26,37 +26,39 @@ NEW_VERSION=$(echo "$VERSIONING_TOOL_OUTPUT" | sed -n 's/\([0-9]*\.[0-9]*\.[0-9]
 NEW_MAJOR_VERSION=$(echo "$NEW_VERSION" | sed -n 's/\([0-9]*\).*/\1/p')
 NEW_MINOR_VERSION=$(echo "$NEW_VERSION" | sed -n 's/[0-9]*\.\([0-9]*\).*/\1/p')
 NEW_PATCH_VERSION=$(echo "$NEW_VERSION" | sed -n 's/[0-9]*\.[0-9]*\.\([0-9]*\)/\1/p')
-if [ "$NEW_VERSION" != "$CURRENT_VERSION" ]; then
-    if [ $NEW_PATCH_VERSION -ne $CURRENT_PATCH_VERSION ]; then
-        SEQFLOW_CALLBACK=$(<$GITHUB_ACTION_PATH/merge.sh)
-        curl -sL https://raw.githubusercontent.com/greg-chuchro/seqflow/v0.0.1/action.sh > $GITHUB_ACTION_PATH/seqflow.sh
-        . $GITHUB_ACTION_PATH/seqflow.sh
-    elif [ $NEW_MINOR_VERSION -ne $CURRENT_MINOR_VERSION ]; then
-        set +e
-        GIT_USER_NAME=$(git config --global user.name)
-        GIT_USER_EMAIL=$(git config --global user.email)
-        set -e
-        git config --global user.name "seqflow-action"
-        git config --global user.email ""
-        git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
-        git fetch
-        
-        LATEST_BRANCH_FULL_NAME=$(git branch --remotes --list origin/v[0-9]*.[0-9]* --sort -version:refname | head -n 1 | xargs)
-        LATEST_BRANCH_NAME=${LATEST_BRANCH_FULL_NAME#"origin/"}
-        LATEST_BRANCH_MAJOR_VERSION=$(echo $LATEST_BRANCH_NAME | sed -n 's/v\([0-9]*\).*/\1/p')
-        LATEST_BRANCH_MINOR_VERSION=$(echo $LATEST_BRANCH_NAME | sed -n 's/v[0-9]*\.\([0-9]*\).*/\1/p')
-        INITIAL_PATCH_VERSION=0
-        NEW_RELEASE_VERSION=$LATEST_BRANCH_MAJOR_VERSION.$(($LATEST_BRANCH_MINOR_VERSION + 1)).$INITIAL_PATCH_VERSION
-        MINOR_BRANCH_NAME=v$LATEST_BRANCH_MAJOR_VERSION.$(($LATEST_BRANCH_MINOR_VERSION + 1))
-        git switch --create $MINOR_BRANCH_NAME
-        sed -i "s/<Version>.*<\/Version>/<Version>$NEW_RELEASE_VERSION<\/Version>/" $(find . -name *.csproj | grep --invert-match Test)
-        git add $(find . -name *.csproj | grep --invert-match Test)
-        git commit --amend --no-edit
-        git push origin $MINOR_BRANCH_NAME
-        eval "$SEQFLOW_MERGE_CALLBACK"
+if [ "$NEW_VERSION" == "$CURRENT_VERSION" ]; then
+    SEQFLOW_CALLBACK=$(<$GITHUB_ACTION_PATH/merge.sh)
+    curl -sL https://raw.githubusercontent.com/greg-chuchro/seqflow/v0.0.1/action.sh > $GITHUB_ACTION_PATH/seqflow.sh
+    . $GITHUB_ACTION_PATH/seqflow.sh
+elif [ $NEW_PATCH_VERSION -ne $CURRENT_PATCH_VERSION ]; then
+    SEQFLOW_CALLBACK=$(<$GITHUB_ACTION_PATH/merge_and_bump.sh)
+    curl -sL https://raw.githubusercontent.com/greg-chuchro/seqflow/v0.0.1/action.sh > $GITHUB_ACTION_PATH/seqflow.sh
+    . $GITHUB_ACTION_PATH/seqflow.sh
+elif [ $NEW_MINOR_VERSION -ne $CURRENT_MINOR_VERSION ]; then
+    set +e
+    GIT_USER_NAME=$(git config --global user.name)
+    GIT_USER_EMAIL=$(git config --global user.email)
+    set -e
+    git config --global user.name "seqflow-action"
+    git config --global user.email ""
+    git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+    git fetch
+    
+    LATEST_BRANCH_FULL_NAME=$(git branch --remotes --list origin/v[0-9]*.[0-9]* --sort -version:refname | head -n 1 | xargs)
+    LATEST_BRANCH_NAME=${LATEST_BRANCH_FULL_NAME#"origin/"}
+    LATEST_BRANCH_MAJOR_VERSION=$(echo $LATEST_BRANCH_NAME | sed -n 's/v\([0-9]*\).*/\1/p')
+    LATEST_BRANCH_MINOR_VERSION=$(echo $LATEST_BRANCH_NAME | sed -n 's/v[0-9]*\.\([0-9]*\).*/\1/p')
+    INITIAL_PATCH_VERSION=0
+    NEW_RELEASE_VERSION=$LATEST_BRANCH_MAJOR_VERSION.$(($LATEST_BRANCH_MINOR_VERSION + 1)).$INITIAL_PATCH_VERSION
+    MINOR_BRANCH_NAME=v$LATEST_BRANCH_MAJOR_VERSION.$(($LATEST_BRANCH_MINOR_VERSION + 1))
+    git switch --create $MINOR_BRANCH_NAME
+    sed -i "s/<Version>.*<\/Version>/<Version>$NEW_RELEASE_VERSION<\/Version>/" $(find . -name *.csproj | grep --invert-match Test)
+    git add $(find . -name *.csproj | grep --invert-match Test)
+    git commit --amend --no-edit
+    git push origin $MINOR_BRANCH_NAME
+    eval "$SEQFLOW_MERGE_CALLBACK"
 
-        git switch $MAIN_BRANCH_NAME
-        git config --global user.name "$GIT_USER_NAME"
-        git config --global user.email "$GIT_USER_EMAIL"
-    fi
+    git switch $MAIN_BRANCH_NAME
+    git config --global user.name "$GIT_USER_NAME"
+    git config --global user.email "$GIT_USER_EMAIL"
 fi
